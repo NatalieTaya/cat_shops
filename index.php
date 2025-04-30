e98fbdaf758e38ab138c4c48d6a51036c7f0f521
Natalia Sergeeva <natasg777@yandex.ru>
2025-04-30 23:21:39 +0300

    slider is added

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="styles/header.css">
     <link rel="stylesheet" href="styles/index.css">
     <link rel="stylesheet" href="styles/footer.css">
-
     <title>Document</title>
 </head>
 <body>
@@ -14,48 +13,66 @@
         include('header.php');
     ?>
     <main>
-    <aside>
-        Расширенный поиск
-        <form action="">
-            <input type="checkbox"> <br>
-            <input type="checkbox"> <br>
-            <input type="checkbox"> <br>
-            <input type="checkbox"> <br>
-            <input type="checkbox"> <br>
-            <input type="checkbox"> <br>
-            <input type="checkbox"> <br>
-            <input type="checkbox"> <br>
-        </form>
-    </aside> 
     <?php
         include('lib/lib_db.php');
+        include('lib/lib_outputHTML.php');
+        error_reporting(0);
+        
+        $max_cost=0;
         if (isset($_POST['name'])) { 
             $products_query = getQueryProducts($_POST['name']);
-            //print_r( $products_query);
-            //print_r( 'Форма работает');
+            for($i=0; $i<sizeof($products_query); $i++){
+                $images[$i] = getQueryPics($products_query[$i]['picture'])[0];
+                // поиск максимума стоимости выведенных товаров
+                if ($products_query[$i]['cost'] > $max_cost) {
+                    $max_cost=$products_query[$i]['cost'];
+                } 
+            }
+            productsHTML($products_query,$images);
         } else {
-            print_r( 'Ничего не найдено');
-        }
-        $images=array();
-        for($i=0; $i<sizeof($products_query); $i++){
-            $images[$i] = getQueryPics($products_query[$i]['picture']);
-        }
-        //print_r(value: $images);
-
-        printf('<div class="products">');
-        for($i=0; $i<sizeof($products_query); $i++){
-            printf('<div class="product">
-                            <h2 class="product_name">%s</h2>
-                            <div class="product_image"><img src="%s" alt=""></div>
-                            <div class="product_cost">%s$</div>
-            </div>', $products_query[$i]['name'], 
-            $images[$i][0]['image'],
-            $products_query[$i]['cost']);
-        }
-        printf('</div>');
-  
-    ?>
+            $products_query = getAllproducts();
+            $images=getAllpics();
+            for($i=0; $i<sizeof($products_query); $i++){
+                // поиск максимума стоимости выведенных товаров
+                if ($products_query[$i]['cost'] > $max_cost) {
+                    $max_cost=$products_query[$i]['cost'];
+                } 
+            }
+        }        
         
+        $costRange=$max_cost;
+        printf('
+            <aside>
+                Расширенный поиск
+                <form method="POST">
+                    <label for="">Цена</label>
+                    <input name="cost_range" type="range" min="0" max="%s" step="1">
+                    <button  type="submit"> Искать </button>
+                </form>', $max_cost);
+            
+        if (isset($_POST['cost_range'])) { 
+            // Получаем значение из диапазона
+            $costRange = $_POST['cost_range'] ?? null;
+            if ($costRange !== null) {
+                $i=0;
+                foreach($products_query as $item) {
+                    // поиск максимума стоимости выведенных товаров
+                    if ($item['cost'] < $costRange) {
+                        $products_filtered[$i]=$item;
+                        $images_filtered[$i] = getQueryPics($item['picture'])[0];
+                        $i++;
+                    } 
+                }
+            } 
+        } 
+        printf('
+            <label for="">Цена: %s</label>
+            </aside> ', $costRange);
+            
+            if($products_filtered !== null) {
+                productsHTML($products_filtered,$images_filtered);
+            } 
+    ?>
     </main>
     <?php
     include('footer.php');
---
+++ lib/lib_outputHTML.php
@@ -0,0 +1,18 @@
+<?php
+
+
+function productsHTML($products_query,$images){
+    
+    printf('<div class="products">');
+            for($i=0; $i<sizeof($products_query); $i++){
+                printf('<div class="product">
+                                <h2 class="product_name">%s</h2>
+                                <div class="product_image"><img src="%s" alt=""></div>
+                                <div class="product_cost">%s$</div>
+                </div>', $products_query[$i]['name'], 
+                $images[$i]['image'],
+                $products_query[$i]['cost']);      
+            }
+    printf('</div>');
+
+}
